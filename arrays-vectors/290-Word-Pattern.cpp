0d4ede9edabab166187567f9444f93af46992702
Areeba Tariq <areebatariq885@gmail.com>
2023-08-21 20:53:27 +0500

    Add 290-Word-Pattern

---
+++ arrays-vectors/290-Word-Pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char,int>m1;
+        map<string,int>m2;
+        istringstream in(s);
+        int i=0,n=pattern.size();
+        for(string word;in>>word;i++)
+        {
+            if(i==n||m1[pattern[i]]!=m2[word])
+            {
+                return false;
+            }
+            m1[pattern[i]]=m2[word]=i+1;
+        }
+        return true;
+
+    }
+};
