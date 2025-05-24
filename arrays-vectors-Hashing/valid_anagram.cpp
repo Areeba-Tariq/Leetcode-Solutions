b78fd584f197bc663721928b1b8d9b95355d7dfd
Areeba Tariq <areebatariq885@gmail.com>
2025-05-25 00:08:27 +0500

    Changes in valid anagram

--- arrays-vectors-Hashing/valid_anagram.cpp
+++ arrays-vectors-Hashing/valid_anagram.cpp
@@ -6,7 +6,7 @@ public:
         {
             return false;
         }
-        int count[26]={0};
+        int count[26]={0}; //if i use char count[26] a test case fails when char cant hold more than 127 value and that no appear more than 127 times so better use int arr
         for(int i=0;i<s.length();i++)
         {
             count[s[i] -'a']++;
@@ -36,3 +36,25 @@ public:
 //to compare every value of s and t)
 //sc=o(n)---(Sorting each string requires O(n) additional space)
 
+class Solution {
+    public:
+        void sortString(string& s) //otherwise pass by value pr wont work
+        {
+            for(int i=0;i<s.size()-1;i++) //bubble sort 
+            {
+                for(int j=i+1;j<s.size();j++)
+                {
+                    if(s[i]>s[j]) swap(s[i],s[j]);
+                }
+            }
+        }
+        bool isAnagram(string s, string t) {
+            //sort strings
+            sortString(s);
+            sortString(t);
+            if(s==t) return true;
+            return false;
+        }
+    };
+    //tc =o(n^2)
+    //sort fun => sort(str.begin(),str.end()) take  O(n log n) time === more effcient than bubble sort
